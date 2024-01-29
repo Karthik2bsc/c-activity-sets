2f4e5c4420909ffc8d815b6a3b34d9b21d62635f
Karthi2bsc <karthikeyanm.bsc23@rvu.edu.in>
2024-01-29 14:15:47 +0530

    test

---
+++ set01/problem04.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void input(int *a, int *b);
+void add(int a, int b, int *sum);
+void output(int a, int b, int sum);
+int main()
+{
+    int a,b,sum;
+    input(&a,&b);
+    add(a,b,&sum);
+    output(a,b,sum);
+    return 0;
+}
+void input(int *a, int *b)
+{
+    printf("enter a: ");
+    scanf("%d",a);
+    printf("enter b: ");
+    scanf("%d",b);
+}
+void add(int a, int b, int *sum)
+{
+    *sum=a+b;
+}
+void output(int a, int b, int sum)
+{
+    printf("sum will be %d",sum);
+}
