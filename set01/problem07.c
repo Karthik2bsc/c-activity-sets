2a463dd729366644b5118f9fb4318ab4ff019ab9
Karthi2bsc <karthikeyanm.bsc23@rvu.edu.in>
2024-01-30 21:24:31 +0530

    test

---
+++ set01/problem07.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+int main()
+{
+    int n=input_n();
+    int sum=sum_n_nos(n);
+    output(n,sum);
+    return 0;
+}
+int input_n()
+{
+    int x;
+    printf("enter x value: ");
+    scanf("%d",&x);
+    return x;
+}
+int sum_n_nos(int n)
+{
+    int sum=0;
+    for(int i=0;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    return sum;
+}
+void output(int n, int sum)
+{
+    printf("sum till %d will be %d",n,sum);
+}
