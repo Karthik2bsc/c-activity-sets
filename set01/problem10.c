c83f42d472cb4b9ef04b93999b82369be5bae147
Karthi2bsc <karthikeyanm.bsc23@rvu.edu.in>
2024-01-31 19:47:17 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,3 +1,43 @@
+#include<stdio.h>
 void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
+int main()
+{
+    char string1[10],string2[10];
+    input_two_strings(string1,string2);
+    int result=stringcompare(string1,string2);
+    output(string1,string2,result);
+    return 0;
+}
+void input_two_strings(char *string1, char *string2)
+{
+    printf("enter the string1: ");
+    scanf("%s",string1);
+    printf("enter the string2: ");
+    scanf("%s",string2);
+}
+int stringcompare(char *string1, char *string2)
+{
+    if(string1>string2)
+    return 0;
+    else if(string1<string2)
+    return 1;
+    else
+    return 2;
+}
+void output(char *string1, char *string2, int result)
+{
+    if(result=0)
+    {
+        printf("%s will be greater than %s",string1,string2);
+    }
+    else if(result=1)
+    {
+        printf("%s will be greater than %s",string2,string1);
+    }
+    else
+    {
+        printf("%s will be equal to %s",string1,string2);
+    }
+}
--- set01/problem11.c
+++ set01/problem11.c
@@ -1,9 +1,38 @@
+#include<stdio.h>
 struct _complex {
 	float real;
-	float imaginary;
+	float img;
 };
 typedef struct _complex Complex;
 
 Complex input_complex();
 Complex add_complex(Complex a, Complex b);
 void output(Complex a, Complex b, Complex sum);
+int main()
+{
+	Complex a=input_complex();
+	Complex b=input_complex();
+	Complex sum=add_complex(a,b);
+	output(a,b,sum);
+	return 0;
+}
+Complex input_complex()
+{
+	Complex n;
+	printf("enter the num: ");
+	scanf("%f",&n.real);
+	printf("enter the num: ");
+	scanf("%f",&n.img);
+	return n;
+}
+Complex add_complex(Complex a, Complex b)
+{
+	Complex sum={0,0};
+	sum.real=a.real+b.real;
+	sum.img=a.img+b.img;
+	return sum;
+}
+void output(Complex a, Complex b, Complex sum)
+{
+	printf("sum will be %f + %fi",sum.real,sum.img);
+}
