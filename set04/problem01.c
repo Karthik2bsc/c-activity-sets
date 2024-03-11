78bf79a05c1a66cbdc52e616d121d4ece497ed96
Karthi2bsc <karthikeyanm.bsc23@rvu.edu.in>
2024-03-11 15:01:08 +0530

    test

---
+++ set04/problem01.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2);
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
+
+void input(int *num1, int *den1, int *num2, int *den2) {
+    printf("Enter num1 and num2: ");
+    scanf("%d %d", num1, num2);
+    printf("Enter den1 and den2: ");
+    scanf("%d %d", den1, den2);
+}
+
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den){
+    *res_num=num1*den2 + num2*den1;
+    *res_den=den1*den2;
+}
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den){
+    printf("sum of %d/%d and %d/%d will be %d/%d",num1, den1, num2, den2, res_num, res_den);
+}
+int main() {
+    int num1, den1, num2, den2, res_num, res_den;
+    input(&num1, &den1, &num2, &den2);
+    add(num1, den1, num2, den2, &res_num, &res_den);
+    output(num1, den1, num2, den2, res_num, res_den);
+    return 0;
+}
