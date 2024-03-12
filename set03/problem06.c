54a0717dfb50e7595bfb4a88efdafb1b3f9b0449
Karthi2bsc <karthikeyanm.bsc23@rvu.edu.in>
2024-03-12 22:09:23 +0530

    test

--- set03/problem06.c
+++ set03/problem06.c
@@ -5,8 +5,8 @@ int sub_str_index(char* string, char* substring);
 void output(char *string, char *substring, int index);
 int main()
 {
-    char a[10],b[10],string[10],substring[10];
-    input_string(a,b);
+    char string[10],substring[10];
+    input_string(string,substring);
     int index=sub_str_index(string,substring);
     output(string,substring,index);
     return 0;
@@ -35,6 +35,6 @@ void output(char *string, char *substring, int index)
     printf("\nsubstring:%s",substring);
     if(index != -1)
     {
-        printf("the index for %s in %s will be %d",substring,string,index);
+        printf("\nthe index for %s in %s will be %d",substring,string,index);
     }
 }
