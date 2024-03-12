7dc8f3ae69613244a8254d93e80f18f45ab3d657
Karthi2bsc <karthikeyanm.bsc23@rvu.edu.in>
2024-03-12 14:22:36 +0530

    test

--- set03/problem05.c
+++ set03/problem05.c
@@ -9,5 +9,41 @@ int main()
     int n=input_array_size();
     int a[n];init_array(n,a);
     erotosthenes_sieve(n,a);
-    output(n,a);return 0;
+    output(n,a);
+    return 0;
+}
+
+int input_array_size()
+{
+    int x;
+    printf("enter the array size: ");
+    scanf("%d",&x);
+    return x;
+}
+void init_array(int n, int a[n]){
+    for(int i=0;i<=n;i++)
+    {
+        a[i]=1;
+    }
+}
+void erotosthenes_sieve(int n, int a[n])
+{
+    for(int p=2;p*p<=n;p++)
+    {
+        if(a[p] == 1)
+        {
+            for(int i=p*p;i<=n;i+=p)
+            {
+                a[i]=0;
+            }
+        }
+    }
+}
+void output(int n, int a[n]){
+    for(int i=2;i<=n;i++)
+    {
+        if(a[i]){
+            printf("%d ",i);
+        }
+    }
 }
--- set03/problem08.c
+++ set03/problem08.c
@@ -10,12 +10,29 @@ typedef struct polygon {
     float perimeter;
 } Polygon;
 
-int input_n();
-Point input_point(char *promt_msg);
-int input_polygon(Polygon *p);
+int input_n(){
+    int x=input_n();
+    printf("enter the no of sides of a polygon: ");
+    scanf("%d",&x);
+    return x;
+}
+Point input_point(char *promt_msg){
+    Point P;
+    for(int i=0;i<=promt_msg;i++){
+    printf("enter the coordinates of point%d",promt_msg);
+    scanf("%f %f",P.x,P.y);
+    }
+    return P;
+}
+int input_polygon(Polygon *p){
+    int p;
+    printf("enter the coordinates of point%d");
+}
 float find_distance(Point a, Point b);
-void find_perimeter(Polygon* p);
+void find_perimeter(Polygon *p);
 void output(Polygon p);
 int main(){
-    
+    Polygon p[100];
+    char promt_msg[100];
+    Point a=input_point(promt_msg);
 }
