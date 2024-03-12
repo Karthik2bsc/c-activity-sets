bda208dde6631095e70bd0b7680a94b678d90923
Karthi2bsc <karthikeyanm.bsc23@rvu.edu.in>
2024-03-12 21:43:16 +0530

    test

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 void input_string(char* a, char* b);
 int sub_str_index(char* string, char* substring);
 void output(char *string, char *substring, int index);
-int main(){
-    char a,b,string,substring;
+int main()
+{
+    char a[10],b[10],string[10],substring[10];
     input_string(a,b);
     int index=sub_str_index(string,substring);
     output(string,substring,index);
@@ -16,7 +18,23 @@ void input_string(char* a, char* b)
     printf("enter the b string: ");
     scanf("%s",b);
 }
-int sub_str_index(char* string, char* substring){
-    int index
+int sub_str_index(char* string, char* substring)
+{
+    char *ptr = strstr(string, substring);
+    if(ptr != NULL)
+    {
+        return ptr - string;
+    }else
+    {
+        return -1;
+    }
 }
-void output(char *string, char *substring, int index);
+void output(char *string, char *substring, int index)
+{
+    printf("string:%s",string);
+    printf("\nsubstring:%s",substring);
+    if(index != -1)
+    {
+        printf("the index for %s in %s will be %d",substring,string,index);
+    }
+}
